7cecd3e195cbeeab171e8c403a3b8879c1433909
dk197 <38752159+dk197@users.noreply.github.com>
2020-01-10 11:30:16 +0100

    add map

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-	<Header></Header>
+	<div>
+		<Header></Header>
+		<Test></Test>
+	</div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Test from './components/charts/test'
 
 export default {
     components: {
-		Header
+		Header,
+		Test
     }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Highcharts from 'highcharts';
+import HighchartsVue from 'highcharts-vue'
+import loadMap from 'highcharts/modules/map.js';
 
 Vue.config.productionTip = false
+Vue.use(HighchartsVue)
+loadMap(Highcharts);
 
 new Vue({
   render: h => h(App),
