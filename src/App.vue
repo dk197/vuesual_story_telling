53a1ce184c728b1d4ed2d78023af605a61b4b5a4
dk197 <38752159+dk197@users.noreply.github.com>
2020-01-10 11:34:16 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
-  <div>
-	<Header></Header>
-  <SectionA></SectionA>
-  <SectionB></SectionB>
-  </div>
+    <div>
+        <Header></Header>
+        <SectionA></SectionA>
+        <SectionB></SectionB>
+        <Test></Test>
+    </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import SectionA from "./components/sections/SectionA.vue";
 import SectionB from "./components/sections/SectionB.vue";
+import Test from "./components/charts/test";
 
 export default {
     components: {
-      Header,
-      SectionA,
-      SectionB
+        Header,
+        SectionA,
+        SectionB,
+        Test
     }
 };
 </script>
@@ -24,16 +27,15 @@ export default {
 body {
     background-color: white;
     margin: 0 !important;
-    font-family: 'Muli';
-    color: #0D1321;
+    font-family: "Muli";
+    color: #0d1321;
 }
 
 .heading {
-    font-family: 'Jost';
+    font-family: "Jost";
     font-size: 100px;
     text-transform: uppercase;
     font-weight: bold;
     width: 550px;
 }
-
 </style>
