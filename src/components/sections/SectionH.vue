67faaa148ddf85fe85aa0e784893d2756df146e6
dk197 <38752159+dk197@users.noreply.github.com>
2020-01-19 16:31:59 +0100

    map functions part1

--- src/components/sections/SectionH.vue
+++ src/components/sections/SectionH.vue
@@ -1,19 +1,72 @@
 <template>
-  <Wrapper>
-    <h2 class="heading">Die Clans</h2>
-    <Map></Map>
-  </Wrapper>
+    <Wrapper>
+        <h2 class="heading">Die Clans</h2>
+        <Map></Map>
+        <StraftatenUndTatverdächtige v-if="showBarChart" :chartData="chartData"></StraftatenUndTatverdächtige>
+    </Wrapper>
 </template>
 
 <script>
 import Wrapper from "../Wrapper.vue";
 import Map from "../charts/map";
+import StraftatenUndTatverdächtige from '../charts/StraftatenUndTatverdächtige'
+import { EventBus } from '../../others/eventBus'
 
 export default {
-  components: {
-    Wrapper,
-    Map
-  }
+    data() {
+        return {
+            showBarChart: false,
+            chartData: {},
+            tableData: {
+                Steinfurt: {
+                    straft: {
+                        year1: 107,
+                        year2: 72,
+                        year3: 74
+                    },
+                    tatverd: {
+                        year1: 67,
+                        year2: 69,
+                        year3: 62
+                    }
+                }
+            }
+        }
+    },
+    components: {
+        Wrapper,
+        Map,
+        StraftatenUndTatverdächtige
+    },
+    mounted() {
+        const x = this
+        EventBus.$on('mapSelected', function(value) {
+            x.getDataOfBezirk(value)
+            x.chartData =  {
+                categories: ['Straftaten', 'Tatverdächtige'],
+                series: [
+                    {
+                        name: '2016',
+                        data: [x.tableData[value].straft.year1, x.tableData[value].tatverd.year1]
+                    },
+                    {
+                        name: '2017',
+                        data: [x.tableData[value].straft.year2, x.tableData[value].tatverd.year2]
+                    },
+                    {
+                        name: '2018',
+                        data: [x.tableData[value].straft.year3, x.tableData[value].tatverd.year3]
+                    }
+                ]
+            }
+            x.showBarChart = true
+        })
+    },
+    methods: {
+        getDataOfBezirk(bezirk) {
+            console.log(this.tableData[bezirk]);
+        }
+    }
 };
 </script>
 
